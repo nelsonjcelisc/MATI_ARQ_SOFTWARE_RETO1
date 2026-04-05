9b5c3c6af34463e2bb4d781500c54f3270d7dfa4
oarestrepol <oarl1@yahoo.com>
2026-04-05 15:31:41 -0500

    Optimizacion tiempos de respuesta en handlerException bd duplicada

--- reto-2/ventas-api/src/main/java/com/uniandes/ventas/web/config/MetricsConfig.java
+++ reto-2/ventas-api/src/main/java/com/uniandes/ventas/web/config/MetricsConfig.java
@@ -1,17 +1,37 @@
 package com.uniandes.ventas.web.config;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @Configuration
 public class MetricsConfig {
-    @Bean(name = "ordenTimer")
-    public Timer ordenTimer(MeterRegistry registry) {
-        return Timer.builder("orden.tiempo.total")
-                .publishPercentiles(0.5, 0.95)
-                .publishPercentileHistogram(true)
-                .register(registry);
+    @Bean
+    public MeterFilter ordenTimerFilter() {
+        return new MeterFilter() {
+            @Override
+            public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+                if (id.getName().equals("orden.tiempo.total")) {
+                    return DistributionStatisticConfig.builder()
+                            .percentiles(0.5, 0.95)
+                            .percentilesHistogram(true)
+                            .serviceLevelObjectives(
+                                    Duration.ofMillis(10).toNanos(),  // Muy rápido
+                                    Duration.ofMillis(50).toNanos(),  // Normal
+                                    Duration.ofMillis(100).toNanos(), // Límite deseado
+                                    Duration.ofMillis(200).toNanos(), // Un poco lento
+                                    Duration.ofMillis(500).toNanos(), // Lento
+                                    Duration.ofSeconds(1).toNanos()   // Alerta crítica
+                            )
+                            .build()
+                            .merge(config);
+                }
+                return config;
+            }
+        };
     }
 }
